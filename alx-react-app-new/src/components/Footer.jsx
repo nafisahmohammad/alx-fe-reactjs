63755907f81dffa425dfcb3c9f1f9f3dd87c8679
nafisahmohammad <nafisahmohammad1616@gmail.com>
2026-01-25 15:30:41 +0100

    Apply inline CSS styling to React components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px' }}>
+      <p>© 2026 My Website</p>
+    </footer>
+  );
+};
+
+export default Footer;
+
