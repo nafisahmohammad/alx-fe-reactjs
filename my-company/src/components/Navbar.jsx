f95b85541305ab0f040b4ddf4a119da06bf39b80
nafisahmohammad <nafisahmohammad1616@gmail.com>
2026-01-25 17:14:53 +0100

    company website

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: 'navy', padding: '10px' }}>
+    <nav
+      style={{
+        backgroundColor: 'navy',
+        padding: '10px',
+        display: 'flex',
+        justifyContent: 'center'
+      }}
+    >
       <Link style={{ color: 'white', margin: '10px' }} to="/">Home</Link>
       <Link style={{ color: 'white', margin: '10px' }} to="/about">About</Link>
       <Link style={{ color: 'white', margin: '10px' }} to="/services">Services</Link>
