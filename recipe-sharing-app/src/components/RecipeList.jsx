e6fffb0894b404906360939159c8a71d8fc0cea7
nafisahmohammad <nafisahmohammad1616@gmail.com>
2026-01-25 17:43:19 +0100

    Enhance recipe app with edit, delete and detailed views using Zustand

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,5 @@
 import { useRecipeStore } from './recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
   const recipes = useRecipeStore(state => state.recipes);
@@ -7,7 +8,11 @@ const RecipeList = () => {
     <div>
       {recipes.map(recipe => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>
+              {recipe.title}
+            </Link>
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}
@@ -16,3 +21,4 @@ const RecipeList = () => {
 };
 
 export default RecipeList;
+
