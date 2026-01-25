f888a4d3fc967081a5dd25dd18f28e229c7523dd
nafisahmohammad <nafisahmohammad1616@gmail.com>
2026-01-25 15:37:34 +0100

    Inline styling

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,10 +2,13 @@ const MainContent = () => {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f2f2f2', minHeight: '200px' }}>
       <h2 style={{ color: 'darkgreen' }}>Welcome to My Page</h2>
-      <p>This is the main content area where information is displayed.</p>
+      <p style={{ fontSize: '16px' }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
     </main>
   );
 };
 
 export default MainContent;
+
 
